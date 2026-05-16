23022229147e42e3c263917a050286a49f8bb9c2
AllyV <vazquezlai988o@gmail.com>
2026-05-15 21:08:40 -0600

    Agrega practica de tipos de datos en JavaScript

---
+++ tiposdedatos.js
@@ -0,0 +1,27 @@
+// Practica de tipos de datos en JavaScript
+// En este archivo estoy practicando el uso de typeof
+// para identificar diferentes tipos de datos en JS
+
+// Number
+console.log(99, "-", typeof 99);
+
+// String
+console.log("Andrelanyenli", "-", typeof "Andrelanyenli");
+
+// Boolean
+console.log(true, "-", typeof true);
+
+// Undefined
+console.log(undefined, "-", typeof undefined);
+
+// Null
+console.log(null, "-", typeof null);
+
+// Object
+console.log({ ciudad: "Mexico" }, "-", typeof { ciudad: "Mexico" });
+
+// Array
+console.log(["HTML", "CSS", "JavaScript"], "-", typeof ["HTML", "CSS", "JavaScript"]);
+
+// BigInt
+console.log(BigInt(2025), "-", typeof BigInt(2025));
